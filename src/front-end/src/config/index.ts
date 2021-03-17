3d492486dbd7b232db640459ac2ed9206be55c07
mrkrustydev <krustydev@protonmail.com>
2021-03-16 19:55:49 -0700

    Update Rewards Per Block

--- src/front-end/src/config/index.ts
+++ src/front-end/src/config/index.ts
@@ -5,7 +5,7 @@ BigNumber.config({
   DECIMAL_PLACES: 80,
 })
 
-export const KRUSTY_PER_BLOCK = new BigNumber(4)
+export const KRUSTY_PER_BLOCK = new BigNumber(2)
 export const BLOCKS_PER_YEAR = new BigNumber(10512000)
 export const BSC_BLOCK_TIME = 3
 export const KRUSTY_POOL_PID = 1

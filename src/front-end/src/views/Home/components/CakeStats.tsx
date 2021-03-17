f215b1b0a90dc47b64fb3d04edfe286743959e6f
mrkrustydev <krustydev@protonmail.com>
2021-03-16 20:13:49 -0700

    Add LP Burn to Stats card

--- src/front-end/src/views/Home/components/CakeStats.tsx
+++ src/front-end/src/views/Home/components/CakeStats.tsx
@@ -5,7 +5,7 @@ import { KRUSTY_PER_BLOCK, BASE_BUY_TOKEN_URL } from 'config/index'
 import { getBalanceNumber } from 'utils/formatBalance'
 import { useTotalSupply, useBurnedBalance } from 'hooks/useTokenBalance'
 import useI18n from 'hooks/useI18n'
-import { getKrustyAddress } from 'utils/addressHelpers'
+import { getKrustyAddress, getKrustyBnbAddress } from 'utils/addressHelpers'
 import CardValue from './CardValue'
 import { Card, CardBody, Heading, Text, LinkExternal } from '../../../pancake-uikit/src'
 import TotalValueLockedCard from './TotalValueLockedCard'
@@ -39,12 +39,9 @@ const StyledLinkExternal = styled(LinkExternal)`
   display: flex;
   align-items: center;
   margin-bottom: 20px;
-  border-radius: 10px;
-  background: ${({ theme }) => theme.colors.secondary};
   padding-top: 5px;
   padding-bottom: 5px;
   padding-left: 10px;
-  padding-right: 10px;
 
   svg {
     padding-left: 4px;
@@ -57,10 +54,12 @@ const StyledLinkExternal = styled(LinkExternal)`
 const CakeStats = () => {
   const TranslateString = useI18n()
   const totalSupply = useTotalSupply()
+  const burnedLpBalance = useBurnedBalance(getKrustyBnbAddress())
   const burnedBalance = useBurnedBalance(getKrustyAddress())
   const krustySupply = totalSupply ? getBalanceNumber(totalSupply) - getBalanceNumber(burnedBalance) : 0
   const krustyPerBlock = new BigNumber(KRUSTY_PER_BLOCK)
   const buyKrustyUrl = `${BASE_BUY_TOKEN_URL}?outputCurrency=${getKrustyAddress()}`
+  const burnWalletLink = 'https://bscscan.com/token/0x577373cc1429655491a7a71d6718133f20f0f3b7?a=0x000000000000000000000000000000000000dead';
 
   return (
     <StyledCakeStats>
@@ -76,6 +75,16 @@ const CakeStats = () => {
           <div style={{width:'auto', display:'inline-flex'}}>
             <CardValue fontSize="14px" value={krustySupply} />
           </div>
+          }
+        </Row>
+        <Row style={{ display: 'inline-flex', width: '100%' }}>
+          <Text fontSize="14px" style={{ width: '40%', display: 'inline-flex' }}>
+            {TranslateString(536, 'Total KRUSTY-BNB LP Burned')}
+          </Text>
+          {krustySupply &&
+            <div style={{ width: 'auto', display: 'inline-flex' }}>
+            <StyledLinkExternal href={burnWalletLink}><CardValue fontSize="14px" value={burnedLpBalance.div(1e18).toNumber()} /></StyledLinkExternal>
+            </div>
           }
         </Row>
         <br/>
